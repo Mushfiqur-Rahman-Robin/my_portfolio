8ee594efd2aeba38573fb14b8051015bd3876f66
Mushfiqur-Rahman-Robin <u1604123@student.cuet.ac.bd>
2025-06-28 12:43:14 +0600

    feat: moved backend to another repository

--- backend/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-# Add the backend root directory (which contains manage.py) to sys.path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
--- backend/gunicorn.conf.py
+++
@@ -1,3 +0,0 @@
-timeout = 90
-workers = 4
-bind = "0.0.0.0:8000"
